ed10589c79345b23dcb792c00dba0f5345340129
Zile995 <stefan.zivkovic995@gmail.com>
2026-07-01 23:27:41 +0200

    refactor(fzf-ctrl-z): optimize job parser and unify input handling
    Signed-off-by: Zile995 <stefan.zivkovic995@gmail.com>

--- zsh-ctrl-z.zsh
+++ zsh-ctrl-z.zsh
@@ -1,44 +1,44 @@
 ZSH_CTRL_Z_HOME="${0:A:h}"
 
 fzf-ctrl-z() {
-  setopt localoptions pipefail no_aliases 2> /dev/null
+  setopt localoptions pipefail no_aliases 2>/dev/null
 
-  local current_jobs=("${(@f)$(jobs -l)}")
-  [[ -n "${current_jobs[1]}" ]] || return
-
-  if [[ ${#current_jobs} -eq 1 ]]; then
+  (( ${#jobstates} == 0 )) && return
+  (( ${#jobstates} == 1 )) && {
     BUFFER=""
-    zle -I && fg >/dev/null 2>&1
+    zle -I && 'builtin' fg >/dev/null 2>&1
     zle reset-prompt
     return
-  fi
+  }
 
-  local selected
   while true; do
-    local current_jobs=("${(@f)$(jobs -l)}")
-    [[ -n "${current_jobs[1]}" ]] || break
+    (( ${#jobstates} )) || break
 
-    selected=$(printf "%s\n" "${current_jobs[@]}" | fzf \
+    local -a current_jobs=("${(@f)$(jobs -l)}")
+    local -a selected=("${(@f)$(printf "%s\n" "${current_jobs[@]}" | fzf \
       --reverse \
+      --expect=ctrl-k \
       --bind "ctrl-z:accept" \
-      --bind "ctrl-k:become(echo kill:{})" \
       --header "ENTER / CTRL-Z: Foreground job | CTRL-K: Kill job" \
-      --prompt="  Search jobs ❯ ")
+      --prompt="  Search jobs ❯ ")}")
 
-    [[ -z $selected ]] && break
+    (( $#selected < 2 )) && break
 
-    if [[ "$selected" =~ "^kill:\[" ]]; then
-      local job_id=$(echo "$selected" | sed 's/kill://' | awk '{print $1}' | tr -d '[]')
-      if [[ -n $job_id ]]; then
-        kill -KILL %"$job_id" 2>/dev/null
-    	sleep 0.1
-      fi
-    else
-      local job_id=$(echo "$selected" | awk '{print $1}' | tr -d '[]')
-      BUFFER=""
-      zle -I && fg %"$job_id" >/dev/null 2>&1
-      break
-    fi
+    local key="${selected[1]}"
+    local job_id="${${${selected[2]%% *}#\[}%\]}"
+    [[ -z $job_id ]] && break
+
+    case "${key:-accept}" in
+      ctrl-k)
+        'builtin' kill -KILL %"$job_id" 2>/dev/null
+        sleep 0.1
+        ;;
+      accept)
+        BUFFER=""
+        zle -I && 'builtin' fg %"$job_id" >/dev/null 2>&1
+        break
+        ;;
+    esac
   done
 
   zle reset-prompt
